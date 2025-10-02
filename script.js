bab4395604002617dc4b7bd3732ed5d739770287
faizan-ahmad5 <fa3n20004@gmail.com>
2025-10-02 22:35:52 +0500

    Add starter project files for collaborative development

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basic starter JavaScript - Developer 3 will enhance this
+
+console.log("🚀 GitHub Collaboration Practice - JavaScript loaded!");
+
+// Wait for DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM loaded - ready for collaboration!");
+});
